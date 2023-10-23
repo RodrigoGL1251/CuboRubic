fd15085ba2964499e2187835d932a60fc713e9d6
RodrigoGL1251 <92412932+RodrigoGL1251@users.noreply.github.com>
2023-10-23 07:41:28 -0600

    Revision de codigo

--- CuboRubic/main.cpp
+++ CuboRubic/main.cpp
@@ -31,14 +31,14 @@ bool m_rotateRubik = false;
 int main(int argc, char** argv)
 {
     t0 = clock();
-    glutInit(&argc, argv);                          //inicia el glut
-    glutInitDisplayMode(GLUT_DEPTH| GLUT_DOUBLE | GLUT_RGBA); //Estable ce la pantalla con doble buffer con los colores rgba
-    glutInitWindowPosition(50, 50);                 //establecemos posicion de la pantalla
-    glutInitWindowSize(500, 500);                   //Establecemos tamaño de la pantalla
-    glutCreateWindow("Cubo Rubik");                 //Nombre de la ventana
-    glEnable(GL_DEPTH_TEST);                        //permite 3d y rotacion
-
-    glutDisplayFunc(display);                       //Dibujamos pantalla
+    glutInit(&argc, argv);                                      //inicia el glut
+    glutInitDisplayMode(GLUT_DEPTH| GLUT_DOUBLE | GLUT_RGB);   //Estable ce la pantalla con doble buffer con los colores rgba
+    glutInitWindowPosition(50, 50);                             //establecemos posicion de la pantalla
+    glutInitWindowSize(500, 500);                               //Establecemos tamaño de la pantalla
+    glutCreateWindow("Cubo Rubik");                             //Nombre de la ventana
+    glEnable(GL_DEPTH_TEST);                                    //permite 3d y rotacion
+    
+    glutDisplayFunc(display);                                   //Dibujamos pantalla
     
     glutMainLoop();
 
@@ -46,35 +46,27 @@ int main(int argc, char** argv)
 }
 
 void specialKey() {
-    
 }
 
 void display(void) {
-    glClearColor(0, 0, 0, 0);           ///Color de fondo
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);       //Borrando pantalla
-    //glMatrixMode(GL_PROJECTION);        //Modo de proyeccion
-    glLoadIdentity();                   //Cargando matriz de identidad
-    //glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);   //Proyeccion ortografica dentro del cubo señalado
-    gluPerspective(60, 1, 1, 100);         //Proyeccion perspectiva dentro del cubo señalado
-    glTranslatef(0, 0, -2);                 //Alejando el cuadrado del observador dos unidades en el eje z
-    glMatrixMode(GL_MODELVIEW);         //Modo de modelado
+    glClearColor(0, 0, 0, 0);                               ///Color de fondo
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);     //Borrando pantalla
+    //glMatrixMode(GL_PROJECTION);                          //Modo de proyeccion
+    glLoadIdentity();                                       //Cargando matriz de identidad
+    //glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);             //Proyeccion ortografica dentro del cubo señalado
+    gluPerspective(60, 1, 1, 100);                          //Proyeccion perspectiva dentro del cubo señalado
+    glTranslatef(0, 0, -2);                                 //Alejando el cuadrado del observador dos unidades en el eje z
+    glMatrixMode(GL_MODELVIEW);                             //Modo de modelado
+    generateCubeRubik(0.8, 0, 0, 0);                        //Genera los 27 cubos del rubik
 
-    m_time = (double(t1 - t0) / CLOCKS_PER_SEC);
-    m_previousTime = m_time;
-    //
-      // m_rubik.rotateAxisX(90 * m_deltaTime, Front);
-        generateCubeRubik(0.8, 0, 0, 0);    //Genera los 27 cubos del rubik
-        m_time = (double(t1 - t0) / CLOCKS_PER_SEC);
-        m_deltaTime = m_time - m_previousTime;
-
-    glFlush();                          //forzando el dibujado
-    glutSwapBuffers();
+    /*glFlush();*/                                          //forzando el dibujado
+    glutSwapBuffers();                                      //cambiando de buffer para que se vea mas fluida(se necesita doble buffer)
 }
 
 void generateCube(float size, float x, float y, float z) {
     float halfSize = size / 2;
     //Front
-    glBegin(GL_POLYGON);                                     //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(0, 0, 1);                                     //color del cuadrado azul
     glVertex3f(x - halfSize, y + halfSize, z + halfSize);   //Coordenadas del vertice superior izquierdo
     glVertex3f(x - halfSize, y - halfSize, z + halfSize);
@@ -82,7 +74,7 @@ void generateCube(float size, float x, float y, float z) {
     glVertex3f(x + halfSize, y + halfSize, z + halfSize);
     glEnd();
     //Left
-    glBegin(GL_POLYGON);                                      //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(1, 0, 0);                                     //color del cuadrado rojo
     glVertex3f(x - halfSize, y + halfSize, z + halfSize);   //Coordenadas del vertice superior izquierdo
     glVertex3f(x - halfSize, y + halfSize, z - halfSize);
@@ -90,7 +82,7 @@ void generateCube(float size, float x, float y, float z) {
     glVertex3f(x - halfSize, y - halfSize, z + halfSize);
     glEnd();
     //Back
-    glBegin(GL_POLYGON);                                      //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(0, 1, 0);                                     //color del cuadrado green
     glVertex3f(x - halfSize, y + halfSize, z - halfSize);   //Coordenadas del vertice superior izquierdo
     glVertex3f(x + halfSize, y + halfSize, z - halfSize);
@@ -98,7 +90,7 @@ void generateCube(float size, float x, float y, float z) {
     glVertex3f(x - halfSize, y - halfSize, z - halfSize);
     glEnd();
     //Right
-    glBegin(GL_POLYGON);                                      //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(1,0.5,1);                                     //color del cuadrado naranja
     glVertex3f(x + halfSize, y + halfSize, z + halfSize);   //Coordenadas del vertice superior izquierdo
     glVertex3f(x + halfSize, y - halfSize, z + halfSize);
@@ -106,15 +98,15 @@ void generateCube(float size, float x, float y, float z) {
     glVertex3f(x + halfSize, y + halfSize, z - halfSize);
     glEnd();
     //Up
-    glBegin(GL_POLYGON);                                      //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(1, 1, 1);                                     //color del cuadrado blanco
-    glVertex3f(x - halfSize, y + halfSize, z +halfSize);   //Coordenadas del vertice superior izquierdo
+    glVertex3f(x - halfSize, y + halfSize, z +halfSize);    //Coordenadas del vertice superior izquierdo
     glVertex3f(x + halfSize, y + halfSize, z +halfSize);
     glVertex3f(x - halfSize, y + halfSize, z -halfSize);
     glVertex3f(x + halfSize, y + halfSize, z -halfSize); 
     glEnd();
     //Down
-    glBegin(GL_POLYGON);                                      //dibujando un cuadrado
+    glBegin(GL_POLYGON);                                    //dibujando un cuadrado
     glColor3f(1, 1, 0);                                     //color del cuadrado amarillo
     glVertex3f(x - halfSize, y - halfSize, z + halfSize);   //Coordenadas del vertice superior izquierdo
     glVertex3f(x + halfSize, y - halfSize, z + halfSize);
